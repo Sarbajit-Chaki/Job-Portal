40914d6341568e8a93ae00c1141149ba9e5fd681
Sarbajit-Chaki <sarbajit.c.0222@inspiria.edu.in>
2024-07-17 21:48:36 +0530

    update

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import {NavLink, Outlet} from 'react-router-dom'
+
+const Navbar = () => {
+    const [profileVisibility, setProfileVisibility] = useState(false)
+
+  return (
+    <>
+        <nav className=' bg-[#f4f4f4] flex justify-around items-center p-3 text-lg font-medium '>
+            <div className=' text-3xl font-bold text-[#00a264] cursor-pointer'>JobPro</div>
+            <div className=' flex items-center gap-x-4'>
+                <NavLink className={(e)=> {return e.isActive?"border-b-4 border-b-[#00a264] text-[#00a264]":""}} to="/">Home</NavLink>
+                <NavLink className={(e)=> {return e.isActive?"border-b-4 border-b-[#00a264] text-[#00a264]":""}} to="/jobs">Jobs</NavLink>
+                <NavLink className={(e)=> {return e.isActive?"border-b-4 border-b-[#00a264] text-[#00a264]":""}} to="/about">About Us</NavLink>
+                <NavLink className={(e)=> {return e.isActive?"border-b-4 border-b-[#00a264] text-[#00a264]":""}} to="/contact">Contact Us</NavLink>
+            </div>
+            <div className= {profileVisibility?"visible cursor-pointer":"invisible"}>Profile</div>
+        </nav>
+        <Outlet />  {/* This will render the child routes */}
+    </>
+  )
+}
+
+export default Navbar
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './components/Home.jsx'
+
+
+const router = createBrowserRouter([  
+  {
+    path: '/',
+    element: <App />, // The main App component
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
   </React.StrictMode>,
 )
